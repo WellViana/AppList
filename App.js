b8e7337ff9c061a87166db0a3f38816a2d73aa0e
Wellington Viana <w.vianasouzaa@gmail.com>
2020-05-14 00:44:22 -0300

    tela cadastro

---
+++ App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ContatosNavigator from './navegacao/ContatosNavigator'
+
+export default function App() {
+    return <ContatosNavigator />;
+}
---
+++ components/ContatoItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+import cores from '../constantes/Cores'
+import medidas from '../medidas/medidas';
+
+const ContatoItem = (props) =>{
+    return (
+        <TouchableOpacity onPress={props.onPress.bind(this, props.contato)} onLongPress={props.onDelete.bind(this, props.contato.key)}>
+            <View style={styles.itemNaLista}>
+                <Text>#{props.contato.key}</Text>
+                <Text>Nome: {props.contato.nome}</Text>
+                <Text>Celular: {props.contato.celular}</Text>
+            </View>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    itemNaLista: {
+        padding: medidas.PEQUENO,
+        backgroundColor: cores.CINZA,
+        borderColor: cores.PRETO,
+        borderWidth: medidas.MINIMO,
+        marginBottom: medidas.PEQUENO,
+        borderRadius: medidas.PEQUENO
+    }
+});
+
+export default ContatoItem;
